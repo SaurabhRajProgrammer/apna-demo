48f5bd688ae556b149bc793716d50277eeb8f7f4
SaurabhRajprogrammer <raj9279907263@gmail.com>
2026-09-16 20:29:25 +0530

    Initial commit

---
+++ List_comprehensions3.py
@@ -0,0 +1,11 @@
+numbers = [1,12,2,4,5,6,7,8,10,14,16,18,22,26,68,98]
+
+even = [num for num in numbers if num%2==0]
+
+print("Even-number:",even)
+
+print("Total_even_numbers:",len(even))
+
+total_number=len(numbers)
+
+print("Total_numbers:",total_number)
