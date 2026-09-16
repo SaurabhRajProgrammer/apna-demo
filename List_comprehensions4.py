1fa22befddb0d48379784b222d9663cd72309e4b
SaurabhRajprogrammer <raj9279907263@gmail.com>
2026-09-16 20:47:56 +0530

    New File

---
+++ List_comprehensions4.py
@@ -0,0 +1,11 @@
+text = "S1A2R3T4I5P9K4E4E86EJ378YRWN"
+
+digits = [ch for ch in text if ch.isdigit()]
+
+print(digits)
+
+numbers = range(1,20)
+
+result = [ n**2 for n in numbers if n % 2 == 0]
+
+print("Even_Squares_num=",result)
